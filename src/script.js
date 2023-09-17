315454594978852fa29afdbf2186535d126d3bbd
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2023-09-17 21:51:37 +0600

    calendar fully functional

--- src/script.js
+++ src/script.js
@@ -34,93 +34,94 @@ function addNotificationDot(buttonId) {
 
 function setupCalendarButtons() {
   const dateElements = document.querySelectorAll('.date');
-  const contentElement = document.getElementById('contentName'); // Assuming you have a single content element
+  const contentElement = document.getElementById('contentName');
 
   dateElements.forEach((element) => {
-      element.addEventListener('click', () => {
-          const hasCustomClass = element.classList.contains('custom');
+    element.addEventListener('click', () => {
+      const hasCustomClass = element.classList.contains('custom');
 
-          dateElements.forEach((date) => {
-              date.classList.remove('custom');
-          });
-
-          if (!hasCustomClass && !element.classList.contains('current-day')) {
-              element.classList.add('custom');
-          }
-
-          // Check if any date has the "custom" class
-          const hasCustomDate = Array.from(dateElements).some((date) => date.classList.contains('custom'));
-
-          // Define an array of dates to match
-          const eeeAssignmentDates = ['16', '24', '25'];
-          const MathCTDates = ['27'];
-          const vacationDates = ['1', '7', '8', '14', '15', '21', '22', '28', '29'];
-
-
-          // Update the display of the content based on the presence of "custom" class
-          if (hasCustomDate) {
-              contentElement.style.display = 'block';
-
-              // Find the date with the "custom" class
-              const customDateElement = Array.from(dateElements).find((date) => date.classList.contains('custom'));
-
-              // Get the date text from the custom date element
-              const customDateText = customDateElement.textContent;
-
-              // Update the content's date with the custom date
-              const contentDateElement = contentElement.querySelector('.card-header.text-light');
-              if (contentDateElement) {
-                  contentDateElement.textContent = customDateText + " September 2023";
-
-                  // Check if customDateText is in the vacationDates array
-                  if (vacationDates.includes(customDateText)) {
-                      // Update the list-group-item innerHTML for vacation dates
-                      const listGroupItem = contentElement.querySelector('.list-group-item.text-light');
-                      if (listGroupItem) {
-                          listGroupItem.innerHTML = "Vacation";
-                      }
-                  }
-                  
-                  else if (eeeAssignmentDates.includes(customDateText)) {
-                    // Update the list-group-item innerHTML for EEE assignment dates
-                    const listGroupItem = contentElement.querySelector('.list-group-item.text-light');
-                    if (listGroupItem) {
-                      listGroupItem.innerHTML = "EEE Assignment";
-                    }
-                  }
-                  
-                  else if (MathCTDates.includes(customDateText)) {
-                    // Update the list-group-item innerHTML for EEE assignment dates
-                    const listGroupItem = contentElement.querySelector('.list-group-item.text-light');
-                    if (listGroupItem) {
-                      listGroupItem.innerHTML = "Math CT";
-                    }
-                  }
-                  
-                  else {
-                      // Reset the list-group-item innerHTML for non-vacation dates
-                      const listGroupItem = contentElement.querySelector('.list-group-item.text-light');
-                      if (listGroupItem) {
-                          listGroupItem.innerHTML = "Usual Class and Lab";
-                      }
-                  }
-              }
-          } else {
-              contentElement.style.display = 'none';
-          }
+      dateElements.forEach((date) => {
+        date.classList.remove('custom');
       });
 
-      // Get the current date
-      const currentDate = new Date();
-      const currentDay = currentDate.getDate();
-
-      // Get the day of the month (1-31)
-      const date = parseInt(element.textContent);
-
-      // Check if the date matches today's date and add the "current-day" class
-      if (date === currentDay) {
-          element.classList.add('current-day');
+      if (!hasCustomClass && !element.classList.contains('current-day')) {
+        element.classList.add('custom');
       }
+
+      // Check if any date has the "custom" class
+      const hasCustomDate = Array.from(dateElements).some((date) => date.classList.contains('custom'));
+
+      // Define an array of dates to match
+      const eeeAssignmentDates = ['16', '24', '20'];
+      const MathCTDates = ['27'];
+      const vacationDates = ['1', '7', '8', '14', '15', '21', '22', '28', '29'];
+      const CTAssignmentDates = ['10', '18', '25'];
+
+      // Update the display of the content based on the presence of "custom" class
+      if (hasCustomDate) {
+        contentElement.style.display = 'block';
+
+        // Find the date with the "custom" class
+        const customDateElement = Array.from(dateElements).find((date) => date.classList.contains('custom'));
+
+        // Get the date text from the custom date element
+        const customDateText = customDateElement.textContent;
+
+        // Update the content's date with the custom date
+        const contentDateElement = contentElement.querySelector('.card-header.text-light');
+        if (contentDateElement) {
+          contentDateElement.textContent = customDateText + " September 2023";
+
+          // Check if customDateText is in the vacationDates array
+          if (vacationDates.includes(customDateText)) {
+            // Update the list-group-item innerHTML for vacation dates
+            const listGroupItem = contentElement.querySelector('.list-group-item.text-light');
+            if (listGroupItem) {
+              listGroupItem.innerHTML = "Vacation";
+            }
+          } else if (eeeAssignmentDates.includes(customDateText)) {
+            // Update the list-group-item innerHTML for EEE assignment dates
+            const listGroup = contentElement.querySelector('.list-group');
+            if (listGroup) {
+              listGroup.innerHTML = `<li class="list-group-item text-light red-list">EEE Assignment</li>`;
+            }
+          } else if (MathCTDates.includes(customDateText)) {
+            // Update the list-group-item innerHTML for Math CT dates
+            const listGroup = contentElement.querySelector('.list-group');
+            if (listGroup) {
+              listGroup.innerHTML = `<li class="list-group-item text-light red-list">Math CT</li>`;
+            }
+          } else if (CTAssignmentDates.includes(customDateText)) {
+            // Update the list-group-item innerHTML for Math CT dates
+            const listGroup = contentElement.querySelector('.list-group');
+            if (listGroup) {
+              listGroup.innerHTML = `<li class="list-group-item text-light red-list">Math CT</li>
+              <li class="list-group-item text-light red-list">Math Assignment</li>`;
+            }
+          } else {
+            // Reset the list-group innerHTML for non-special dates
+            const listGroup = contentElement.querySelector('.list-group');
+            if (listGroup) {
+              listGroup.innerHTML = `<li class="list-group-item text-light red-list">Usual Class and Lab</li>`;
+            }
+          }
+        }
+      } else {
+        contentElement.style.display = 'none';
+      }
+    });
+
+    // Get the current date
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
+
+    // Get the day of the month (1-31)
+    const date = parseInt(element.textContent);
+
+    // Check if the date matches today's date and add the "current-day" class
+    if (date === currentDay) {
+      element.classList.add('current-day');
+    }
   });
 }
 
